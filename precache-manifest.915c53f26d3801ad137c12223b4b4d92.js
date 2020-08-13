6919ddf1edceb76e9bccfaf7010117230464b61a
Marina-Melikhova <marinamelihova29@gmail.com>
2020-08-13 17:56:36 +0300

    Updates

---
+++ precache-manifest.915c53f26d3801ad137c12223b4b4d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f0b05e568df0f54014f3e3dbeaeefe",
+    "url": "/goit-react-hw-01-feedback/index.html"
+  },
+  {
+    "revision": "67fc2765954c53e6f96b",
+    "url": "/goit-react-hw-01-feedback/static/css/main.2d82cb85.chunk.css"
+  },
+  {
+    "revision": "cd6bab529dcab9af0c23",
+    "url": "/goit-react-hw-01-feedback/static/js/2.fd24358d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-feedback/static/js/2.fd24358d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fc2765954c53e6f96b",
+    "url": "/goit-react-hw-01-feedback/static/js/main.2d261390.chunk.js"
+  },
+  {
+    "revision": "f54ee1e78f22ac49313c",
+    "url": "/goit-react-hw-01-feedback/static/js/runtime-main.576c983e.js"
+  }
+]);
